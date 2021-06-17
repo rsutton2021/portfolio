22962a5d8bbe982ec1d6329e8ae49184fb8ccbdd
Rob Sutton <rsutton.code@gmail.com>
2021-06-17 18:09:36 +0100

    Add pop-up framework

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+/*===================================== 
+======== MENU CLOSE FUNCTION ==========
+=====================================*/
 function toggle() {
     let menuClose = document.getElementById('nav-toggle');
     menuClose.checked = false;
 }
+
+/*===================================== 
+======== PROJECTS INFO POPUP ==========
+=====================================*/
+
