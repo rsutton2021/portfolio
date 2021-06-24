94374bd949347bfdf5f19b00058d5fab89a60fdb
Rob Sutton <rsutton.code@gmail.com>
2021-06-24 13:04:35 +0100

    Add popup JS code

--- main.js
+++ main.js
@@ -10,3 +10,32 @@ function toggle() {
 ======== PROJECTS INFO POPUP ==========
 =====================================*/
 
+const openPopupButtons = document.querySelectorAll('[data-modal-target]');
+const closePopupButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('popup-overlay');
+
+openPopupButtons.forEach (button => {
+    button.addEventListener('click', () => {
+        const popup = document.querySelector(button.dataset.modalTarget)
+        openModal(popup)
+    })
+});
+
+overlay.addEventListener('click', () => {
+    const popups = document.querySelectorAll('.modal.active')
+    popups.forEach(popup => {
+        closePopupButtons(popup)
+    })
+});
+
+function openPopup(popup) {
+    if (popup === null) return
+    popup.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closePopup(popup) {
+    if (popup === null) return
+    popup.classList.remove('active')
+    overlay.classList.remove('active')
+}
